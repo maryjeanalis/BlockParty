b540fe235fe4b2dffbc49dd6bad8278a6007b98d
mary jean alis <alismaryjean@gmail.com>
2018-04-24 09:52:17 +0800

    asjdhashdkasd

--- BlockParty/AppDelegate.h
+++ BlockParty/AppDelegate.h
@@ -7,10 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+vxfxxfgfxfgfh
+//@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
 
 
 @end
